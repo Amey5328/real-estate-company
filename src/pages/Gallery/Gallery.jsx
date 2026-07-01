cba16e8979e9627159641032e1e89f459c04cd1a
Vishwajeet Kapurakr <vishwajeetkapurkar@gmail.com>
2026-07-01 16:22:48 +0530

    Created gallery page structure and hero section

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,5 +1,24 @@
+import "./Gallery.css";
+
 const Gallery = () => {
-  return <h1>Gallery Page</h1>;
+  return (
+    <main className="gallery-page">
+<p>vishwajeet working on gallery page  </p>
+      {/* Hero Section */}
+      <section className="gallery-hero">
+
+        <h1 className="gallery-title">
+          Gallery
+        </h1>
+
+        <p className="gallery-breadcrumb">
+          Home / Gallery
+        </p>
+
+      </section>
+
+    </main>
+  );
 };
 
 export default Gallery;
